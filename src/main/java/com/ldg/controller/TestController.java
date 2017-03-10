033284bd610a9a371ae3cf4260c10ee0843322fb
liudongguang <liudongguang@qq.com>
2017-03-10 17:17:04 +0800

    Initial commit

---
+++ src/main/java/com/ldg/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ldg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by liudo on 2017/3/10 0010.
+ */
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+    @RequestMapping(value = "/ok")
+    public String ok() {
+        System.out.println("okkkkkkkkk----------->");
+        return "/index.jsp";
+    }
+}
